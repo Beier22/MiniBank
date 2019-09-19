24b3610bac6d95dc5594337febd420516c315fd9
meganpoon <meganpoonwl@gmail.com>
2019-09-19 10:34:25 +0200

    hi

--- MiniBank.Core.Entity/Customer.cs
+++ MiniBank.Core.Entity/Customer.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace MiniBank.Core.Entity
 {
-    class Customer
+    public class Customer
     {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
     }
 }
